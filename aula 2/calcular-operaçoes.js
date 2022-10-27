b3c45c7842894f12ecd0f9dd10e9d73c38cb4736
eduarda <eduardabrotto@outlook.com>
2022-10-27 08:58:03 -0300

    continuação das aulas

---
+++ aula 2/calcular-operaçoes.js
@@ -0,0 +1,16 @@
+/* DESAFIO: faça um programa que salva dois numeros e em seguida realiza as seguintes operações:
+soma, subtração, divisão, módulo e multiplicação e exiba no seu console. */
+
+const primeiroNumero = 4
+const segundoNumero = 5
+
+console.log(`a soma de ${primeiroNumero} com ${segundoNumero} é igual a ${primeiroNumero + segundoNumero}`)
+console.log(`a divisão de ${primeiroNumero} com ${segundoNumero} é igual a ${primeiroNumero / segundoNumero}`)
+console.log(`a subtração de ${primeiroNumero} com ${segundoNumero} é igual a ${primeiroNumero - segundoNumero}`)
+console.log(`a multiplicação de ${primeiroNumero} com ${segundoNumero} é igual a ${primeiroNumero * segundoNumero}`)
+
+/* respostas: 
+a soma de 4 com 5 é igual a 9
+a divisão de 4 com 5 é igual a 0.8
+a subtração de 4 com 5 é igual a -1
+a multiplicação de 4 com 5 é igual a 20 */
---
+++ aula 2/circunferencia.js
@@ -0,0 +1,8 @@
+/* crie um programa para medir o comprimento da circunferência
+comprimento = 2*PI*R sendo PI = 3.1415 e R = 8 */
+
+const numeroPI = 3.1415
+const letraR = 8
+const comprimentoCircunferencia = 2 * numeroPI * letraR
+
+console.log(`o comprimento da circunferencia é igual a ${comprimentoCircunferencia}`)
